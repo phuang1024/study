41f3a6b0e7e52b3685f41bd9ca47b3321f4f3b75
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2022-05-31 19:03:58 -0700

    Can set words properly.

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-console.log("hi");
+let correct_ind;  // Correct choice index
 
-let data = fetch("./content/test.json").then(response => response.json());
-data.then(function(result) {
-    console.log(result);
-})
+/**
+ * Start inclusive, end exclusive.
+ */
+function randint(start, end) {
+    return Math.floor(Math.random() * (end-start)) + start;
+}
+
+function getWrongs(length, correct) {
+    const choices = [];
+    while (choices.length < 3) {
+        let index = randint(0, length);
+        if (index == correct || choices.includes(index))
+            continue;
+        choices.push(index);
+    }
+
+    return choices;
+}
+
+
+function setChoice(index, content) {
+    document.getElementById("word"+(index+1)).innerHTML = content;
+}
+
+function setContent() {
+    let json = fetch("./content/test.json").then(response => response.json());
+    json.then(function(data) {
+        correct_ind = randint(0, 4);
+        let correct = randint(0, data.length);
+        let wrongs = getWrongs(data.length, correct);
+
+        document.getElementById("ans").innerHTML = data[correct][0];
+
+        let next = 0;
+        for (let i = 0; i < 4; i++) {
+            if (i == correct_ind) {
+                setChoice(i, data[correct][0]);
+            } else {
+                setChoice(i, data[wrongs[next]][1]);
+                next++;
+            }
+        }
+    })
+}
