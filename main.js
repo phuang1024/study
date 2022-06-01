19428828b8c6f5b588d7e0caa6b4b8f454c95602
Patrick Huang <huangpatrick16777216@gmail.com>
2022-05-31 21:44:10 -0700

    Random translate direction.

--- main.js
+++ main.js
@@ -41,20 +41,23 @@ function initialize() {
 function setContent() {
     let json = fetch("./content/" + category + ".json").then(response => response.json());
     json.then(function(data) {
+        let zero = randint(0, 2);
+        let one = (zero == 0) ? 1 : 0;
+
         correct_ind = randint(0, 4);
         let correct = randint(0, data.length);
         let wrongs = getWrongs(data.length, correct);
 
-        document.getElementById("ans").innerHTML = data[correct][0];
-        user_prompt = data[correct][0];
-        user_correct = data[correct][1];
+        document.getElementById("ans").innerHTML = data[correct][zero];
+        user_prompt = data[correct][zero];
+        user_correct = data[correct][one];
 
         let next = 0;
         for (let i = 0; i < 4; i++) {
             if (i == correct_ind) {
-                setChoice(i, data[correct][1]);
+                setChoice(i, data[correct][one]);
             } else {
-                setChoice(i, data[wrongs[next]][1]);
+                setChoice(i, data[wrongs[next]][one]);
                 next++;
             }
         }
